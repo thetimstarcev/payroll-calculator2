0d346637f203c5dadffeda43d3f14db0d2ae6ada
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-04-21 10:54:42 -0400

    Completed exercises 3. Displayed the employee names by calling the employee's getName()

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,54 @@
 package com.pluralsight;
 
 public class Employee {
+    private int id;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+/**
+    public Employee(int id, String name, double hoursWorked, double payRate) {
+        this.id = id;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    } **/
+
+
+
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/employees.csv"));
+            bufReader.readLine(); // skips header
+            String line;
+
+            while ((line = bufReader.readLine()) != null) {
+
+                String [] parts = line.split ("\\|");
+
+                int id = Integer.parseInt(parts [0]);
+                String name = parts [1];
+                double hoursWorked = Double.parseDouble(parts [2]);
+                double payRate = Double.parseDouble(parts [3]);
+
+
+                Employee employee = new Employee();
+                employee.setId(id);
+                employee.setName(name);
+                employee.setHoursWorked(hoursWorked);
+                employee.setPayRate(payRate);
+
+                System.out.println(employee.getName());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+                }
+            }
 }
+
