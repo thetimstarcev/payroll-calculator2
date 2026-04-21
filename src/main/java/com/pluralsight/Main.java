7aadf18eed26c73ee44c339b5d33b17519cacdba
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-04-21 14:25:29 -0400

    Wrote the payroll report to employeesnew.csv file

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,21 +1,28 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the file employee file to process: ");
+        String selectedFile = scanner.nextLine();
+
+        System.out.println("Enter the name of the payroll file to create: ");
+        String payrollFile = scanner.nextLine();
+
 
         try {
-            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/employees.csv"));
-            bufReader.readLine(); // skips header
+            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/employeesnew.csv"));
+            bufReader.readLine();// skips header
+            FileWriter writer = new FileWriter(payrollFile);
+            writer.write("id|name|hours-worked|pay-rate\n");
+
             String line;
-
             while ((line = bufReader.readLine()) != null) {
-
+                // printEmployee (line); create method here
                 String [] parts = line.split ("\\|");
 
                 int id = Integer.parseInt(parts [0]);
@@ -31,10 +38,14 @@ public class Main {
                 employee.setPayRate(payRate);
 
                 System.out.println(employee.getId() + " " + employee.getName() + " " + employee.calculateGrossPay());
+
+                //write new name
+                writer.write (employee.getId() + "|" + employee.getName() + "|" + employee.calculateGrossPay() + "\n");
             }
+            writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
                 }
-            }
+    }
 }
 
